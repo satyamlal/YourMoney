b982f53b01f5db42eb3e5b0b384a2fedf8684341
Satyam Lal <ssatyamlal@gmail.com>
2023-03-19 13:36:11 +0530

    initial commit

---
+++ app/src/main/java/com/devsphere/yourmoney/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.devsphere.yourmoney.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Surface = Color.Black
+val TextPrimary = Color.White
+val Destructive = Color(0xFFFF453A)
+val Primary = Color(0xFF0A84FF)
+
+val SystemGray04 = Color(0xFF3A3A3C)
+val BackgroundElevated = Color(0xFF1C1C1E)
+val LabelSecondary = Color(0xFF8E8E93)
+val DividerColor = Color(0x20787880)
+val TopAppBarBackground = Color(0x5E1D1D1D)
+val FillTertiary = Color(0x18767680)

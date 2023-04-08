455736d3a777db69cc3d9ed065f1e114ab1a9fe0
ssatyamlal <ssatyamlal@gmail.com>
2023-04-08 13:02:29 +0530

    working on categories viewmodel

--- app/src/main/java/com/devsphere/yourmoney/pages/Categories.kt
+++ app/src/main/java/com/devsphere/yourmoney/pages/Categories.kt
@@ -1,40 +1,46 @@
 package com.devsphere.yourmoney.pages
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material.icons.rounded.KeyboardArrowLeft
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.devsphere.yourmoney.Greeting
 import com.devsphere.yourmoney.components.TableRow
 import com.devsphere.yourmoney.ui.theme.BackgroundElevated
 import com.devsphere.yourmoney.ui.theme.DividerColor
 import com.devsphere.yourmoney.ui.theme.Shapes
 import com.devsphere.yourmoney.ui.theme.TopAppBarBackground
+import com.devsphere.yourmoney.viewmodels.CategoriesViewModel
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Categories(navController: NavController) {
+fun Categories(navController: NavController, vm : CategoriesViewModel = viewModel()) {
+    val uiState by vm.uiState.collectAsState()
+
+    val colorPickerController = rememberColorPickerController()
+    colorPickerController.selectedColor.value
+
     Scaffold(topBar = {
         MediumTopAppBar(title = { Text("Categories") },
             colors = TopAppBarDefaults.mediumTopAppBarColors(
                 containerColor = TopAppBarBackground
             ),
             navigationIcon = {
-                Surface(onClick = navController::popBackStack,
-                color = Color.Transparent) {
+                Surface(
+                    onClick = navController::popBackStack, color = Color.Transparent
+                ) {
                     Row(modifier = Modifier.padding(vertical = 10.dp, horizontal = 5.dp)) {
                         Icon(
                             Icons.Rounded.KeyboardArrowLeft,
@@ -44,7 +50,7 @@ fun Categories(navController: NavController) {
                     }
                 }
             })
-    }, content = { innerPadding ->
+    },content = { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             Column(
                 modifier = Modifier
@@ -53,15 +59,40 @@ fun Categories(navController: NavController) {
                     .background(BackgroundElevated)
                     .fillMaxWidth()
             ) {
-                TableRow("Categories", hasArrow = true, modifier = Modifier.clickable {
-                    navController.navigate("settings/categories")
-                })
+                TableRow("Groceries")
                 Divider(
                     modifier = Modifier.padding(start = 16.dp, end = 16.dp),
                     thickness = 1.dp,
                     color = DividerColor
                 )
-                TableRow("Erase all data", isDestructive = true)
+
+                TableRow("Bills")
+                Divider(
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                    thickness = 1.dp,
+                    color = DividerColor
+                )
+
+                TableRow("Fruits")
+                Divider(
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                    thickness = 1.dp,
+                    color = DividerColor
+                )
+
+                TableRow("Vegetables")
+                Divider(
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                    thickness = 1.dp,
+                    color = DividerColor
+                )
+            }
+            Row() {
+                if(uiState.colorPickerShowing) {
+                    Dialog(onDismissRequest = { /*TODO*/ }) {
+                        
+                    }
+                }
             }
         }
     })
---
+++ app/src/main/java/com/devsphere/yourmoney/viewmodels/CategoriesViewModel.kt
@@ -0,0 +1,61 @@
+package com.devsphere.yourmoney.viewmodels
+
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import com.devsphere.yourmoney.ui.theme.Primary
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+
+data class CategoriesState(
+    val newCategoryColor: Color = Primary,
+    val newCategoryName: String = "",
+    val colorPickerShowing: Boolean = false,
+)
+
+
+class CategoriesViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(CategoriesState())
+    val uiState: StateFlow<CategoriesState> = _uiState.asStateFlow()
+
+    fun setNewCategoryColor(color: Color) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                newCategoryColor = color
+            )
+        }
+    }
+
+    fun setNewCategoryName(name: String) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                newCategoryName = name
+            )
+        }
+    }
+
+    fun showColorPicker(){
+        _uiState.update {currentState ->
+            currentState.copy(
+                colorPickerShowing = true,
+            )
+
+        }
+    }
+
+    fun hideColorPicker(){
+        _uiState.update {currentState ->
+            currentState.copy(
+                colorPickerShowing = false,
+            )
+
+        }
+    }
+
+    fun createNewCategory() {
+        //TODO: save new category to local db
+    }
+}
